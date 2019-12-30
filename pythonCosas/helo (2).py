7ae8bab6541827b2a6372dea689845a71e29f1ec
Bardo1 <walterrmz1@gmail.com>
2019-12-29 21:34:37 -0300

    Pruebas

---
+++ pythonCosas/helo (2).py
@@ -0,0 +1,8 @@
+print ("hello world")
+print("¿Cómo se llama?")
+nombre = input()
+print(f"Me alegro de conocerle, {nombre}")
+
+numero1 = int(input("Dígame un número: "))
+numero2 = int(input(f"Dígame un número mayor que {numero1}: "))
+print(f"La diferencia entre ellos es {numero2 - numero1}.")
